36ef991d3d2a01a40d231908ff6d0e4aafa8c648
ryoma-android <takahashiryouma0102@gmail.com>
2021-07-28 19:30:29 +0700

    Qa

---
+++ app/src/main/java/com/example/qa_app/Const.kt
@@ -0,0 +1,12 @@
+package com.example.qa_app
+
+const val UsersPATH = "users"
+
+// Firebaseに質問を保存するバス
+const val ContentsPATH = "contents"
+
+// Firebaseに回答を保存するパス
+const val AnswersPATH = "answers"
+
+// Preferenceに表示名を保存する時のキー
+const val NameKEY = "name"
---
+++ app/src/main/java/com/example/qa_app/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.qa_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private var mGenre = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { _ ->
+
+            val user = FirebaseAuth.getInstance().currentUser
+
+            if (user == null) {
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.app_name,
+            R.string.app_name
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.nav_hobby) {
+            toolbar.title = getString(R.string.menu_hobby_label)
+            mGenre = 1
+        } else if (id == R.id.nav_life) {
+            toolbar.title = getString(R.string.menu_life_label)
+            mGenre = 2
+        } else if (id == R.id.nav_health) {
+            toolbar.title = getString(R.string.menu_health_label)
+            mGenre = 3
+        } else if (id == R.id.nav_compter) {
+            toolbar.title = getString(R.string.menu_compter_label)
+            mGenre = 4
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
+
+
